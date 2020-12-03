f4a3d2a78afc511432d21e553806a4822e00f80f
LinhChi1502 <linhchidkc@gmail.com>
2020-12-03 11:14:53 +0700

    add

---
+++ src/main/java/com/example/demo/config/AppSecConfig.java
@@ -0,0 +1,48 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class AppSecConfig  extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(userBuilder.username("chi").password("123456").roles("USER").build());
+        manager.createUser(userBuilder.username("toan").password("123456").roles("ADMIN").build());
+        return manager;
+    }
+
+
+//        @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("123456").roles("ADMIN")
+//            .and()
+//            .withUser("user").password("12345").roles("USER");
+//    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
+                .and()
+                .authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.csrf().disable();
+    }
+
+}
--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,8 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("")
+    @GetMapping("/")
+    public String index(){
+        return "time-line";
+    }
+    @GetMapping("/admin")
     public String home(){
         return "notifications";
     }
+
+    @GetMapping("/user")
+    public String facebook(){
+        return "facebook";
+    }
+
+    @GetMapping("/admin/detail")
+    public String detail(){
+        return "home";
+    }
+
 }
